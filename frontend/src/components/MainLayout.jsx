2688083dee3680e5493257ade169c71384d75998
AryanMittal11 <mittalaryan1110@gmail.com>
2025-05-19 16:29:34 +0530

    sidebar

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import LeftSidebar from './LeftSidebar'
 
 function MainLayout() {
   return (
     <div>
-        SideBar
+        <LeftSidebar/>
         <div>
             <Outlet/>
         </div>
