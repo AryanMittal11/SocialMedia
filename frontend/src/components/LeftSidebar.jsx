a8eebbeddbaa0a7ad04446c13179513f75df39f2
AryanMittal11 <mittalaryan1110@gmail.com>
2025-05-20 13:34:17 +0530

    redux

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -4,54 +4,20 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { toast } from 'sonner'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-
-const sidebarItems = [
-    {
-        icon: <Home />,
-        text: "Home"
-    },
-    {
-        icon: <Search />,
-        text: "Search"
-    },
-    {
-        icon: <TrendingUp />,
-        text: "Explore"
-    },
-    {
-        icon: <MessageCircle />,
-        text: "Messages"
-    },
-    {
-        icon: <Heart />,
-        text: "Notifications"
-    },
-    {
-        icon: <PlusSquare />,
-        text: "Create"
-    },
-    {
-        icon: (
-            <Avatar className='w-6 h-6'>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-        ),
-        text: "Profile"
-    },
-    {
-        icon: <LogOut />,
-        text: "Logout"
-    },
-]
+import { useDispatch, useSelector } from 'react-redux'
+import store from '@/redux/store'
+import { setAuthUser } from '@/redux/authSlice'
 
 function LeftSidebar() {
     const navigate = useNavigate();
+    const { user } = useSelector(store => store.auth);
+    const dispatch = useDispatch();
 
     const logoutHandler = async () => {
         try {
-            const res = await axios.get('http://localhost:8000/api/v1/user/logout', {withCredentials:true});
+            const res = await axios.get('http://localhost:8000/api/v1/user/logout', { withCredentials: true });
             if (res.data.success) {
+                dispatch(setAuthUser(null));
                 navigate("/login");
                 toast.success(res.data.message);
             }
@@ -62,6 +28,47 @@ function LeftSidebar() {
     const sidebarHandler = (textType) => {
         if (textType == 'Logout') logoutHandler();
     }
+
+    const sidebarItems = [
+        {
+            icon: <Home />,
+            text: "Home"
+        },
+        {
+            icon: <Search />,
+            text: "Search"
+        },
+        {
+            icon: <TrendingUp />,
+            text: "Explore"
+        },
+        {
+            icon: <MessageCircle />,
+            text: "Messages"
+        },
+        {
+            icon: <Heart />,
+            text: "Notifications"
+        },
+        {
+            icon: <PlusSquare />,
+            text: "Create"
+        },
+        {
+            icon: (
+                <Avatar className='w-6 h-6'>
+                    <AvatarImage src={user?.profilePicture} />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            ),
+            text: "Profile"
+        },
+        {
+            icon: <LogOut />,
+            text: "Logout"
+        },
+    ]
+
     return (
         <div className='fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen'>
             <div className='flex flex-col'>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import { toast } from 'sonner';
 import { Link, useNavigate } from 'react-router-dom';
 import { Loader2 } from 'lucide-react';
+import { useDispatch } from 'react-redux';
+import { setAuthUser } from '@/redux/authSlice';
 
 function Login() {
     const [input, setInput] = useState({
@@ -14,6 +16,7 @@ function Login() {
 
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const changeEventHandler = (e) => {
         setInput({ ...input, [e.target.name]: e.target.value })
@@ -31,6 +34,7 @@ function Login() {
                 withCredentials: true
             });
             if (res.data.success) {
+                dispatch(setAuthUser(res.data.user));
                 navigate("/")
                 toast.success(res.data.message);
                 setInput({
